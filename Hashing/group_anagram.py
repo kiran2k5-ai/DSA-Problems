406c21e0b8ee571696efcc3db13a8f578f2a0537
kiran-2005 <kiranmrp1310@gmail.com>
2025-08-06 01:07:39 +0530

    Update group_anagram.py

--- Hashing/group_anagram.py
+++ Hashing/group_anagram.py
@@ -1,1 +1,14 @@
+Group Anagrams
 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        grp_ana={}
+
+        for i in strs:
+            s=''.join(sorted(i))
+            if s not in grp_ana:
+                grp_ana.update({s:[i]})
+            else:
+                grp_ana[s].append(i)
+        return list(grp_ana.values())
