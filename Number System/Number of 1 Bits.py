f28c99c8b99b9f53c69dcf6efbd8f4838b4c0736
kiran-2005 <kiranmrp1310@gmail.com>
2025-11-28 13:01:24 +0530

    Implement Hamming Weight function in Solution class

---
+++ Number System/Number of 1 Bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        
+        count = 0
+        while n > 0:
+            rem = n % 2
+            if rem == 1:
+                count += 1
+            n //= 2
+        return count
